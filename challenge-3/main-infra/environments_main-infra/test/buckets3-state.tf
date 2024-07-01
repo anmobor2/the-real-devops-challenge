d8a0f4d53ec45ae143e77b64148a4f4692b3a6e2
Antonio  Moreno Borras <tonibandal@hotmail.com>
2024-07-02 00:14:04 +0200

    comment this file's code after the first run

--- challenge-3/main-infra/environments_main-infra/test/buckets3-state.tf
+++ challenge-3/main-infra/environments_main-infra/test/buckets3-state.tf
@@ -27,73 +27,72 @@ data "aws_iam_policy_document" "terraform_state_policy" {
 }
 
 # AFTER THE FIST TIME RUNNING THIS CODE, COMMENT OUT
-resource "aws_iam_policy" "terraform_state_policy" {
-  name   = "TerraformStatePolicy"
-  policy = data.aws_iam_policy_document.terraform_state_policy.json
+# resource "aws_iam_policy" "terraform_state_policy" {
+#   name   = "TerraformStatePolicy"
+#   policy = data.aws_iam_policy_document.terraform_state_policy.json
 
-}
+# }
 
 data "aws_caller_identity" "current" {}
 
-#AFTER THE FIST TIME RUNNING THIS CODE, COMMENT OUT
-resource "aws_iam_role" "terraform_state_role" {
-  name = "TerraformStateRole"
+# AFTER THE FIST TIME RUNNING THIS CODE, COMMENT OUT
+# resource "aws_iam_role" "terraform_state_role" {
+#   name = "TerraformStateRole"
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
+#   assume_role_policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "Service": "ec2.amazonaws.com"
+#       },
+#       "Action": "sts:AssumeRole"
+#     }
+#   ]
+# }
+# EOF
+# }
 
-#AFTER THE FIST TIME RUNNING THIS CODE, COMMENT OUT
-resource "aws_iam_role_policy_attachment" "terraform_state_role_policy_attachment" {
-  role       = aws_iam_role.terraform_state_role.name
-  policy_arn = aws_iam_policy.terraform_state_policy.arn
-}
+# AFTER THE FIST TIME RUNNING THIS CODE, COMMENT OUT
+# resource "aws_iam_role_policy_attachment" "terraform_state_role_policy_attachment" {
+#   role       = aws_iam_role.terraform_state_role.name
+#   policy_arn = aws_iam_policy.terraform_state_policy.arn
+# }
 
-#AFTER THE FIST TIME RUNNING THIS CODE, COMMENT OUT
-resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket        = "terraform-state-bucket-unique-id-toni"
-  force_destroy = true
-  tags = {
-    Name = "Terraform State Bucket"
-  }
+# AFTER THE FIST TIME RUNNING THIS CODE, COMMENT OUT
+# resource "aws_s3_bucket" "terraform_state_bucket" {
+#   bucket        = "terraform-state-bucket-unique-id-toni"
+#   force_destroy = true
+#   tags = {
+#     Name = "Terraform State Bucket"
+#   }
 
-  #AFTER THE FIST TIME RUNNING THIS CODE, COMMENT OUT
-  depends_on = [
-    aws_iam_role_policy_attachment.terraform_state_role_policy_attachment,
-    aws_dynamodb_table.terraform_state_lock
-  ]
+# AFTER THE FIST TIME RUNNING THIS CODE, COMMENT OUT
+  # depends_on = [
+  #   aws_iam_role_policy_attachment.terraform_state_role_policy_attachment,
+  #   aws_dynamodb_table.terraform_state_lock
+  # ]
 
-  lifecycle {
-    ignore_changes = [
-      bucket,
-    ]
-  }
-}
+#   lifecycle {
+#     ignore_changes = [
+#       bucket,
+#     ]
+#   }
+# }
 
-#AFTER THE FIST TIME RUNNING THIS CODE, COMMENT OUT
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "terraform-state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+# resource "aws_dynamodb_table" "terraform_state_lock" {
+#   name         = "terraform-state-lock"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
 
-  tags = {
-    Name = "Terraform State Lock Table"
-  }
-}
+#   tags = {
+#     Name = "Terraform State Lock Table"
+#   }
+# }
